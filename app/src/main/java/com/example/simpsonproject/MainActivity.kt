9d67b81284b6f0a2caad1b21690f6c9e19e3718f
ricardo.machorro <ricardo.machorro@infotec.mx>
2026-02-22 19:37:04 -0600

    Inclusion of buttons main screen

--- app/src/main/java/com/example/simpsonproject/MainActivity.kt
+++ app/src/main/java/com/example/simpsonproject/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.simpsonproject.components.mainScreen.MainScreen
 import com.example.simpsonproject.ui.theme.SimpsonProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SimpsonProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MainScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpsonProjectTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/simpsonproject/components/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/simpsonproject/components/mainScreen/MainScreen.kt
@@ -1,20 +1,26 @@
 package com.example.simpsonproject.components.mainScreen
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.simpsonproject.ui.theme.*
 
 
 @Composable
@@ -24,21 +30,28 @@ fun MainScreen(){
 
     Box(
         modifier = Modifier
-            .background(Color(0xFFe0e637))
+            .background(BackGroundColor)
             .fillMaxSize()
+
     ){
 
         Column(
             modifier = Modifier
-                .padding(16.dp)
+                .padding(top =75.dp, bottom = 75.dp)
+                .fillMaxSize()
         )
         {
             LazyRow (
-
+               modifier = Modifier
+                   .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Absolute.SpaceAround
             ){
 
                 items( listaOpcciones){ it ->
                         Button(
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = MainColorButton, // Set the desired background color
+                                contentColor = Color.Black),
                             onClick = {}
                         ){
                             Text(it)
