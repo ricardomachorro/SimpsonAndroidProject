785c1ba4e94796c91475df51aded15bcfc5db7d8
ricardo.machorro <ricardo.machorro@infotec.mx>
2026-02-24 20:06:33 -0600

    diseño pagina principal

--- app/src/main/java/com/example/simpsonproject/components/mainScreen/MainScreen.kt
+++ app/src/main/java/com/example/simpsonproject/components/mainScreen/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.simpsonproject.components.mainScreen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -7,19 +8,31 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.simpsonproject.R
 import com.example.simpsonproject.ui.theme.*
 
 
@@ -37,13 +50,16 @@ fun MainScreen(){
 
         Column(
             modifier = Modifier
-                .padding(top =75.dp, bottom = 75.dp)
-                .fillMaxSize()
+                .padding(horizontal =25.dp, vertical = 75.dp)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
         )
         {
             LazyRow (
                modifier = Modifier
-                   .fillMaxWidth(),
+                   .fillMaxWidth()
+                   .padding(vertical = 10.dp),
                 horizontalArrangement = Arrangement.Absolute.SpaceAround
             ){
 
@@ -58,6 +74,40 @@ fun MainScreen(){
                         }
                 }
             }
+
+            ElevatedCard(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 6.dp
+                ),
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Text("Welcome to the Simpson App where you can see all about the program",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center)
+
+
+                    Image(
+                        modifier = Modifier
+                            .padding(vertical = 80.dp)
+                            .size(230.dp),
+                        painter = painterResource(R.drawable.homer_simpson),
+                        contentDescription = ""
+                    )
+                }
+
+
+
+            }
+
 
 
 
