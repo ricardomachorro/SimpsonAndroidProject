07e9b3a535904e522b438aa07f77257ff0444c87
ricardo.machorro <ricardo.machorro@infotec.mx>
2026-03-11 21:24:56 -0600

    correccion diseños cards

--- app/src/main/java/com/example/simpsonproject/components/characterScreen/CharacterScreen.kt
+++ app/src/main/java/com/example/simpsonproject/components/characterScreen/CharacterScreen.kt
@@ -1,12 +1,15 @@
 package com.example.simpsonproject.components.characterScreen
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -30,31 +33,35 @@ fun CharacterScreen(){
      val characterList by viewModel.characterList
 
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(vertical =30.dp)
-    ) {
-
         LazyColumn (modifier = Modifier
-            .padding(horizontal = 20.dp)){
+            .padding(horizontal = 20.dp, vertical =30.dp)){
 
             items(characterList) {
                     element ->
 
                 Card(
+                    elevation= CardDefaults.cardElevation(8.dp),
+                    shape= RoundedCornerShape(16.dp),
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(vertical =40.dp)
+                        .padding(vertical =20.dp)
                 ){
-                    Text(element.name)
+                    Column(
+                        modifier = Modifier.padding(16.dp),
+                    ){
+                        Text("Name: ${element.name}")
+                        Text("Age: ${element.age}")
+                        Text("Gender: ${element.gender}")
+
+                    }
+
                 }
 
 
             }
         }
 
-    }
+
 
 
 }
